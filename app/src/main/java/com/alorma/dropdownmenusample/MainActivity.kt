91ca80fc08f1b9fc789b3ce6962d6303684fb982
Bernat Borrás <bernat.borras@adevinta.com>
2021-09-21 19:52:26 +0200

    Use DropdownIconMenu

--- app/src/main/java/com/alorma/dropdownmenusample/MainActivity.kt
+++ app/src/main/java/com/alorma/dropdownmenusample/MainActivity.kt
@@ -3,23 +3,15 @@ package com.alorma.dropdownmenusample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import com.alorma.dropdownmenusample.components.DropdownIconMenu
+import com.alorma.dropdownmenusample.components.MenuItem
 import com.alorma.dropdownmenusample.ui.theme.DropdownMenuSampleTheme
 import kotlinx.coroutines.launch
 
@@ -60,27 +52,9 @@ class MainActivity : ComponentActivity() {
     onItem1Click: () -> Unit,
     onItem2Click: () -> Unit,
   ) {
-    var showMenu by remember { mutableStateOf(false) }
-
-    IconButton(onClick = { showMenu = true }) {
-      Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
-    }
-    DropdownMenu(
-      expanded = showMenu,
-      onDismissRequest = { showMenu = false },
-    ) {
-      DropdownMenuItem(onClick = {
-        showMenu = false
-        onItem1Click()
-      }) {
-        Text(text = "Item 1")
-      }
-      DropdownMenuItem(onClick = {
-        showMenu = false
-        onItem2Click()
-      }) {
-        Text(text = "Item 2")
-      }
+    DropdownIconMenu {
+      MenuItem(onClick = onItem1Click) { Text(text = "Item 1") }
+      MenuItem(onClick = onItem2Click) { Text(text = "Item 2") }
     }
   }
 }
---
+++ app/src/main/java/com/alorma/dropdownmenusample/components/DropdownMenuIcon.kt
@@ -0,0 +1,89 @@
+package com.alorma.dropdownmenusample.components
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MenuDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+interface DropdownScope : ColumnScope {
+  fun dismiss()
+}
+
+class DropdownScopeImpl(
+  private val columnScope: ColumnScope,
+  private val dismissCallback: () -> Unit = { }
+) : DropdownScope, ColumnScope by columnScope {
+
+  override fun dismiss() {
+    dismissCallback()
+  }
+}
+
+@Composable
+fun DropdownIconMenu(
+  content: @Composable DropdownScope.() -> Unit,
+) {
+  var showPopup by remember { mutableStateOf(false) }
+
+  val onDismiss: () -> Unit = { showPopup = false }
+
+  Box {
+    IconButton(
+      onClick = { showPopup = true }
+    ) {
+      Icon(
+        imageVector = Icons.Default.MoreVert,
+        contentDescription = null,
+        tint = LocalContentColor.current.copy(alpha = ContentAlpha.medium)
+      )
+    }
+    DropdownMenu(
+      expanded = showPopup,
+      onDismissRequest = onDismiss,
+      content = {
+        DropdownScopeImpl(
+          columnScope = this,
+          dismissCallback = onDismiss,
+        ).content()
+      },
+    )
+  }
+}
+
+@Composable
+fun DropdownScope.MenuItem(
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  contentPadding: PaddingValues = MenuDefaults.DropdownMenuItemContentPadding,
+  interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+  content: @Composable RowScope.() -> Unit
+) {
+  DropdownMenuItem(
+    onClick = {
+      onClick()
+      dismiss()
+    },
+    modifier = modifier,
+    enabled = enabled,
+    contentPadding = contentPadding,
+    interactionSource = interactionSource,
+    content = content,
+  )
+}
