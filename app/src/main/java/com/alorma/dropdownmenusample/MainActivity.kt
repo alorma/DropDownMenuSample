879e8acd67be54dbf52ee161abb7991af6fd4c8d
Bernat Borrás <bernat.borras@adevinta.com>
2021-09-21 19:47:51 +0200

    Show dropdown menu with 2 items

--- app/src/main/java/com/alorma/dropdownmenusample/MainActivity.kt
+++ app/src/main/java/com/alorma/dropdownmenusample/MainActivity.kt
@@ -3,6 +3,8 @@ package com.alorma.dropdownmenusample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -10,6 +12,11 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import com.alorma.dropdownmenusample.ui.theme.DropdownMenuSampleTheme
 
@@ -23,15 +30,45 @@ class MainActivity : ComponentActivity() {
             TopAppBar(
               title = { Text(stringResource(R.string.app_name)) },
               actions = {
-                IconButton(onClick = { /*TODO*/ }) {
-                  Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
-                }
+                MoreAction(
+                  onItem1Click = {},
+                  onItem2Click = {}
+                )
               }
             )
           }
         ) {
 
         }
+      }
+    }
+  }
+
+  @Composable
+  private fun MoreAction(
+    onItem1Click: () -> Unit,
+    onItem2Click: () -> Unit,
+  ) {
+    var showMenu by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { showMenu = true }) {
+      Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
+    }
+    DropdownMenu(
+      expanded = showMenu,
+      onDismissRequest = { showMenu = false },
+    ) {
+      DropdownMenuItem(onClick = {
+        showMenu = false
+        onItem1Click()
+      }) {
+        Text(text = "Item 1")
+      }
+      DropdownMenuItem(onClick = {
+        showMenu = false
+        onItem2Click()
+      }) {
+        Text(text = "Item 2")
       }
     }
   }
